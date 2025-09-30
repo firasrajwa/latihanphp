34c1ea0631cc9109fc03f22df2c32f7ab6364d12
firasrajwa <firasrajwa24@gmail.com>
2025-09-30 22:24:30 +0700

    latihanphp

---
+++ Kasusperulangan.php
@@ -0,0 +1,17 @@
+<?php
+$nilai = 75; // misalnya nilai yang mau dicek
+
+if ($nilai >= 0 && $nilai <= 10) {
+    echo "Nilai Huruf: E";
+} elseif ($nilai >= 11 && $nilai <= 50) {
+    echo "Nilai Huruf: D";
+} elseif ($nilai >= 51 && $nilai <= 60) {
+    echo "Nilai Huruf: C";
+} elseif ($nilai >= 61 && $nilai <= 79) {
+    echo "Nilai Huruf: B";
+} elseif ($nilai >= 80 && $nilai <= 100) {
+    echo "Nilai Huruf: A";
+} else {
+    echo "Nilai tidak valid!";
+}
+?>
